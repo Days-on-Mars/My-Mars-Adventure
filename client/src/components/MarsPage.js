24c433bc4f8526de3c09c04aee5f3b7bde7a9319
Hanlin Li <lihanlinapp@gmail.com>
2021-07-26 18:30:09 +0200

    form and routes completed accoriding to plan, codes can be optimized, sanity check to be done tmr

--- client/src/components/MarsPage.js
+++ client/src/components/MarsPage.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { Dropdown, DropdownButton } from 'react-bootstrap';
+import EditJournal from './EditJournal'
 
 //const toDo = [oneToDo, twoToDo, threeToDo, fourToDo, fiveToDo, sixToDo, sevenToDo, eightToDo, nineToDo, tenToDo]
 export default class MarsPage extends Component {
     state = {
         apod: [],
         weather: [],
+        article: {},
         paragrah: '',
         oneToDo: '',
         oneStatus: '',
@@ -28,7 +30,8 @@ export default class MarsPage extends Component {
         nineToDo: '',
         nineStatus: '',
         tenToDo: '',
-        tenStatus: ''
+        tenStatus: '',
+        editModeEnabled: true
     }
 
     getApodData = () => {
@@ -59,6 +62,28 @@ export default class MarsPage extends Component {
         this.getWeatherData();
 	}
 
+    
+    
+    handleChange = event => {
+		const { name, value } = event.target;
+		this.setState({
+			[name]: value
+		})
+	}
+
+    // toggleEditForm = () => {
+	// 	this.setState(state => ({
+	// 		editForm: !state.editForm
+	// 	}))
+	// }
+    handleAddClick() {
+        this.setState({ editModeEnabled: !this.state.editModeEnabled });
+      }
+
+    // handleEditClick() {
+    //     this.setState({ editModeEnabled: !this.state.editModeEnabled });
+    //   }
+
     handleSubmit = event => {
 		event.preventDefault();
 		// make a post request to the server
@@ -85,8 +110,9 @@ export default class MarsPage extends Component {
             tenToDo: this.state.tenToDo,
             tenStatus: this.state.tenStatus
 		})
-			.then(() => {
+			.then(response => {
 				this.setState({
+            article: response.data,
 			paragrah: this.state.paragrah,
             oneToDo: this.state.oneToDo,
             oneStatus: this.state.oneStatus,
@@ -111,19 +137,78 @@ export default class MarsPage extends Component {
 				})
 				// trigger getData() in Projects.js to retrieve the current list
 				// of projects from the server
-				this.props.getData();
+				//this.props.getData();
 			})
 			.catch(err => console.log(err))
 	}
-    
-    
-    handleChange = event => {
-		const { name, value } = event.target;
-		this.setState({
-			[name]: value
+
+    handleEditSubmit = event => {
+		event.preventDefault();
+        const { paragrah, oneToDo, oneStatus, twoToDo, twoStatus, threeToDo, threeStatus, fourToDo, fourStatus, fiveToDo, fiveStatus, sixToDo, sixStatus, sevenToDo, sevenStatus, eightToDo, eightStatus, nineToDo, nineStatus, tenToDo, tenStatus } = this.state;
+
+		axios.post(`/mars-journal/${this.state.article._id}`, {
+			paragrah,
+            oneToDo,
+            oneStatus,
+            twoToDo,
+            twoStatus,
+            threeToDo,
+            threeStatus,
+            fourToDo,
+            fourStatus,
+            fiveToDo,
+            fiveStatus,
+            sixToDo,
+            sixStatus,
+            sevenToDo,
+            sevenStatus,
+            eightToDo,
+            eightStatus,
+            nineToDo,
+            nineStatus,
+            tenToDo,
+            tenStatus
 		})
+			.then(response => {
+				this.setState({
+            updatedJournal: response.data,
+			paragrah: response.data.paragrah,
+            oneToDo: response.data.oneToDo,
+            oneStatus: response.data.oneStatus,
+            twoToDo: response.data.twoToDo,
+            twoStatus: response.data.twoStatus,
+            threeToDo: response.data.threeToDo,
+            threeStatus: response.data.threeStatus,
+            fourToDo: response.data.fourToDo,
+            fourStatus: response.data.fourStatus,
+            fiveToDo: response.data.fiveToDo,
+            fiveStatus: response.data.fiveStatus,
+            sixToDo: response.data.ixToDo,
+            sixStatus: response.data.sixStatus,
+            sevenToDo: response.data.sevenToDo,
+            sevenStatus: response.data.sevenStatus,
+            eightToDo: response.data.eightToDo,
+            eightStatus: response.data.eightStatus,
+            nineToDo: response.data.nineToDo,
+            nineStatus: response.data.nineStatus,
+            tenToDo: response.data.tenToDo,
+            tenStatus: response.data.tenStatus
+				})
+				// trigger getData() in Projects.js to retrieve the current list
+				// of projects from the server
+				//this.props.getData();
+			})
+			.catch(err => console.log(err))
 	}
 
+    deleteProject = () => {
+		// delete the project in the database
+		axios.delete(`/mars-journal/${this.state.article._id}`)
+			.then(() => {
+				this.props.history.push('/mars-journal/');
+			})
+			.catch(err => console.log(err))
+	}
 
     render() {
         const  picture  = this.state.apod;
@@ -151,6 +236,7 @@ export default class MarsPage extends Component {
 					name="paragrah"
 					value={this.state.name}
 					onChange={this.handleChange}
+                    disabled={!this.state.editModeEnabled}
 				/>
                 <label htmlFor="oneToDo">Task 1 </label>
 				<input
@@ -159,15 +245,174 @@ export default class MarsPage extends Component {
 					name="oneToDo"
 					value={this.state.name}
 					onChange={this.handleChange}
+                    disabled={!this.state.editModeEnabled}
 				/>
-                <select name="oneStatus" defaultValue={this.state.selectValue} 
-                onChange={this.handleChange}>
+                <select name="oneStatus" value={this.state.selectValue} 
+                onChange={this.handleChange} disabled={!this.state.editModeEnabled}>
+                    <option value="Status">Current Status</option>
                     <option value="To do">To do</option>
                     <option value="In Progress">In Progress</option>
                     <option value="Completed">Completed</option>
                 </select>
-				<button type="submit">Add for Today</button>
-			</form>
+
+                <label htmlFor="twoToDo">Task 2 </label>
+				<input
+					type="text"
+					id="twoToDo"
+					name="twoToDo"
+					value={this.state.name}
+					onChange={this.handleChange}
+                    disabled={!this.state.editModeEnabled}
+				/>
+                <select name="twoStatus" value={this.state.selectValue} 
+                onChange={this.handleChange} disabled={!this.state.editModeEnabled}>
+                    <option value="Status">Current Status</option>
+                    <option value="To do">To do</option>
+                    <option value="In Progress">In Progress</option>
+                    <option value="Completed">Completed</option>
+                </select>
+
+                <label htmlFor="threeToDo">Task 3 </label>
+				<input
+					type="text"
+					id="threeToDo"
+					name="threeToDo"
+					value={this.state.name}
+					onChange={this.handleChange}
+                    disabled={!this.state.editModeEnabled}
+				/>
+                <select name="threeStatus" value={this.state.selectValue} 
+                onChange={this.handleChange} disabled={!this.state.editModeEnabled}>
+                    <option value="Status">Current Status</option>
+                    <option value="To do">To do</option>
+                    <option value="In Progress">In Progress</option>
+                    <option value="Completed">Completed</option>
+                </select>
+
+                <label htmlFor="fourToDo">Task 4 </label>
+				<input
+					type="text"
+					id="fourToDo"
+					name="fourToDo"
+					value={this.state.name}
+					onChange={this.handleChange}
+                    disabled={!this.state.editModeEnabled}
+				/>
+                <select name="fourStatus" value={this.state.selectValue} 
+                onChange={this.handleChange} disabled={!this.state.editModeEnabled}>
+                    <option value="Status">Current Status</option>
+                    <option value="To do">To do</option>
+                    <option value="In Progress">In Progress</option>
+                    <option value="Completed">Completed</option>
+                </select>
+
+                <label htmlFor="fiveToDo">Task 5 </label>
+				<input
+					type="text"
+					id="fiveToDo"
+					name="fiveToDo"
+					value={this.state.name}
+					onChange={this.handleChange}
+                    disabled={!this.state.editModeEnabled}
+				/>
+                <select name="fiveStatus" value={this.state.selectValue} 
+                onChange={this.handleChange} disabled={!this.state.editModeEnabled}>
+                    <option value="Status">Current Status</option>
+                    <option value="To do">To do</option>
+                    <option value="In Progress">In Progress</option>
+                    <option value="Completed">Completed</option>
+                </select>
+
+                <label htmlFor="sixToDo">Task 6 </label>
+				<input
+					type="text"
+					id="sixToDo"
+					name="sixToDo"
+					value={this.state.name}
+					onChange={this.handleChange}
+                    disabled={!this.state.editModeEnabled}
+				/>
+                <select name="sixStatus" value={this.state.selectValue} 
+                onChange={this.handleChange} disabled={!this.state.editModeEnabled}>
+                    <option value="Status">Current Status</option>
+                    <option value="To do">To do</option>
+                    <option value="In Progress">In Progress</option>
+                    <option value="Completed">Completed</option>
+                </select>
+
+                <label htmlFor="sevenToDo">Task 7 </label>
+				<input
+					type="text"
+					id="sevenToDo"
+					name="sevenToDo"
+					value={this.state.name}
+					onChange={this.handleChange}
+                    disabled={!this.state.editModeEnabled}
+				/>
+                <select name="sevenStatus" value={this.state.selectValue} 
+                onChange={this.handleChange} disabled={!this.state.editModeEnabled}>
+                    <option value="Status">Current Status</option>
+                    <option value="To do">To do</option>
+                    <option value="In Progress">In Progress</option>
+                    <option value="Completed">Completed</option>
+                </select>
+
+                <label htmlFor="eightToDo">Task 8 </label>
+				<input
+					type="text"
+					id="eightToDo"
+					name="eightToDo"
+					value={this.state.name}
+					onChange={this.handleChange}
+                    disabled={!this.state.editModeEnabled}
+				/>
+                <select name="eightStatus" value={this.state.selectValue} 
+                onChange={this.handleChange} disabled={!this.state.editModeEnabled}>
+                    <option value="Status">Current Status</option>
+                    <option value="To do">To do</option>
+                    <option value="In Progress">In Progress</option>
+                    <option value="Completed">Completed</option>
+                </select>
+
+                <label htmlFor="nineToDo">Task 9 </label>
+				<input
+					type="text"
+					id="nineToDo"
+					name="nineToDo"
+					value={this.state.name}
+					onChange={this.handleChange}
+                    disabled={!this.state.editModeEnabled}
+				/>
+                <select name="nineStatus" value={this.state.selectValue} 
+                onChange={this.handleChange} disabled={!this.state.editModeEnabled}>
+                    <option value="Status">Current Status</option>
+                    <option value="To do">To do</option>
+                    <option value="In Progress">In Progress</option>
+                    <option value="Completed">Completed</option>
+                </select>
+
+                <label htmlFor="tenToDo">Task 10 </label>
+				<input
+					type="text"
+					id="tenToDo"
+					name="tenToDo"
+					value={this.state.name}
+					onChange={this.handleChange}
+                    disabled={!this.state.editModeEnabled}
+				/>
+                <select name="tenStatus" value={this.state.selectValue} 
+                onChange={this.handleChange} disabled={!this.state.editModeEnabled}>
+                    <option value="Status">Current Status</option>
+                    <option value="To do">To do</option>
+                    <option value="In Progress">In Progress</option>
+                    <option value="Completed">Completed</option>
+                </select>
+
+				<button type="submit" onClick={this.handleAddClick.bind(this)} >Add</button>
+            </form>
+            <button onClick={this.handleAddClick.bind(this)}>Edit</button>
+            <button onClick={this.handleEditSubmit} type="submit">Re-submit</button>
+            <button onClick={this.deleteProject}>Forget about it, I am off today 🗑</button>
             </div>
         )
     }
--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,70 @@ const axios = require('axios');
 const User = require('../models/User')
 const UserInput = require('../models/UserInput')
 
-//rendering homepage, no need to log-in
-router.get("/", (req, res, next) => {
-    res.render('HomePage');
+//no need to render or redirect any pages, this is done in front-end
+
+
+//creating user entries
+router.post('/mars-journal', (req, res, next) => {
+	// console.log(req.body);
+	const { paragrah, oneToDo, oneStatus, twoToDo, twoStatus, threeToDo, threeStatus, fourToDo, fourStatus, fiveToDo, fiveStatus, sixToDo, sixStatus, sevenToDo, sevenStatus, eightToDo, eightStatus, nineToDo, nineStatus, tenToDo, tenStatus } = req.body;
+	const loggedInUser = req.user;
+	console.log("1st creating user journal and to-do list entry");
+	console.log("2nd checking user ID:", loggedInUser._id);
+	//here use user.findbyId so we know which user is adding the journal
+	UserInput.create({ paragrah, oneToDo, oneStatus, twoToDo, twoStatus, threeToDo, threeStatus, fourToDo, fourStatus, fiveToDo, fiveStatus, sixToDo, sixStatus, sevenToDo, sevenStatus, eightToDo, eightStatus, nineToDo, nineStatus, tenToDo, tenStatus })
+	.then(createdJournal => {
+		User.findByIdAndUpdate(loggedInUser._id, {"$push": {"journaladded": createdJournal}}, {new: true})
+		.then(userFound => {
+		console.log(`This journal has just been added: ${createdJournal}`);
+		console.log(`/UserInput/${createdJournal._id}`);
+		console.log(`Checking if we find a user: ${userFound}`);
+		res.status(201).json(createdJournal)
+	})
+	//is this actually needed?
+})
 });
 
 
+router.post('/mars-journal/:id', (req, res, next) => {
+	console.log('test if this is running');
+	const { paragrah, oneToDo, oneStatus, twoToDo, twoStatus, threeToDo, threeStatus, fourToDo, fourStatus, fiveToDo, fiveStatus, sixToDo, sixStatus, sevenToDo, sevenStatus, eightToDo, eightStatus, nineToDo, nineStatus, tenToDo, tenStatus } = req.body;
+	const loggedInUser = req.user;
+	UserInput.findByIdAndUpdate(req.params.id, { paragrah, oneToDo, oneStatus, twoToDo, twoStatus, threeToDo, threeStatus, fourToDo, fourStatus, fiveToDo, fiveStatus, sixToDo, sixStatus, sevenToDo, sevenStatus, eightToDo, eightStatus, nineToDo, nineStatus, tenToDo, tenStatus }, { new: true })
+	  .then(updatedJournal => {
+		res.status(200).json(updatedJournal);
+	  })
+	  .catch(err => {
+		next(err);
+	  })
+  });
 
-// You put the next routes here 👇
-// example: router.use("/auth", authRoutes)
+  router.delete('/mars-journal/:id', (req, res, next) => {
+	UserInput.findByIdAndDelete(req.params.id)
+	  .then(() => {
+		res.status(200).json({ message: 'journal deleted' });
+	  })
+	  .catch(err => {
+		next(err);
+	  })
+  });
+  
+
+//get relevent user entries from the database, this is user-specific
+router.get('/dashboard', (req, res, next) => {
+    const loggedInUser = req.user;
+	console.log("checking if receiving user info:", loggedInUser._id);
+    User.findById(loggedInUser._id)
+        .populate('journaladded')
+        .then(userFromDB =>
+		//	userFromDB => {
+		//	console.log("checking info from DB:", userFromDB)
+        //   res.render('Dashboard', { user: userFromDB });
+		res.status(200).json(userFromDB)
+        )
+        .catch(err => {
+			console.log(err);
+		})
+  });
 
 module.exports = router;
