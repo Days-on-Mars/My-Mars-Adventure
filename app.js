236db580f1d2c4c62ecb81676c7b0ceadb453ebb
joannakot <joanna.kot.ok@gmail.com>
2021-07-26 10:21:27 +0200

    styling prop for login added

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ const app = express();
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const DB_URL = 'mongodb://localhost/daysonmars';
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const DB_URL = process.env.MONGODB_URI || "mongodb://localhost/daysonmars";
 
 // session configuration
 app.use(
@@ -26,56 +26,56 @@ app.use(
 		saveUninitialized: false,
 		resave: true,
 		store: MongoStore.create({
-			mongoUrl: DB_URL
-		})
+			mongoUrl: DB_URL,
+		}),
 	})
-)
+);
 // end of session configuration
 
 // passport configuration
 // http://www.passportjs.org/docs/configure/
-const User = require('./models/User');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcrypt');
+const User = require("./models/User");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
 
 // this method is used by passport to put the id of the user into the session
 passport.serializeUser((user, done) => {
 	done(null, user._id);
-})
+});
 
 // this is used to retrieve the user by it's id (that is stored in the session)
 passport.deserializeUser((id, done) => {
 	User.findById(id)
-		.then(dbUser => {
-			done(null, dbUser)
+		.then((dbUser) => {
+			done(null, dbUser);
 		})
-		.catch(err => {
+		.catch((err) => {
 			done(err);
-		})
-})
+		});
+});
 
 passport.use(
 	new LocalStrategy((username, password, done) => {
 		// this logic will be executed when we log in
 		User.findOne({ username: username })
-			.then(userFromDB => {
+			.then((userFromDB) => {
 				if (userFromDB === null) {
 					// there is no user with this username
-					done(null, false, { message: 'Something is wrong' });
+					done(null, false, { message: "Something is wrong" });
 				} else if (!bcrypt.compareSync(password, userFromDB.password)) {
 					// the password does not match
-					done(null, false, { message: 'Something is wrong' });
+					done(null, false, { message: "Something is wrong" });
 				} else {
 					// everything correct - user should be logged in
 					done(null, userFromDB);
 				}
 			})
-			.catch(err => {
+			.catch((err) => {
 				next(err);
-			})
+			});
 	})
-)
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
--- client/src/components/MarsPage.js
+++ client/src/components/MarsPage.js
@@ -1,61 +1,73 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-
+import React, { Component } from "react";
+// import { Link } from 'react-router-dom';
+import axios from "axios";
 
 export default class MarsPage extends Component {
-    state = {
-        apod: [],
-        weather: []
-    }
+	state = {
+		apod: [],
+		weather: [],
+	};
 
-    getApodData = () => {
-		axios.get('https://api.nasa.gov/planetary/apod?api_key=rWoTXXeBvjPqFXL8VgQVicRTI2BLUNTmRdpbPOke')
-			.then(response => {
+	getApodData = () => {
+		axios
+			.get(
+				"https://api.nasa.gov/planetary/apod?api_key=rWoTXXeBvjPqFXL8VgQVicRTI2BLUNTmRdpbPOke"
+			)
+			.then((response) => {
 				console.log("1st checking apod api returning data:", response.data.url);
 				this.setState({
-					apod: response.data.url
-				})
+					apod: response.data.url,
+				});
 			})
-			.catch(err => console.log(err))
-	}
+			.catch((err) => console.log(err));
+	};
 
-    getWeatherData = () => {
-		axios.get('https://api.nasa.gov/DONKI/FLR?startDate=2021-07-10&endDate=2021-07-22&api_key=rWoTXXeBvjPqFXL8VgQVicRTI2BLUNTmRdpbPOke&feedtype=json&ver=1.0')
-			.then(response => {
+	getWeatherData = () => {
+		axios
+			.get(
+				"https://api.nasa.gov/DONKI/FLR?startDate=2021-07-10&endDate=2021-07-22&api_key=rWoTXXeBvjPqFXL8VgQVicRTI2BLUNTmRdpbPOke&feedtype=json&ver=1.0"
+			)
+			.then((response) => {
 				console.log("2nd checking weather api returning data:", response);
 				this.setState({
-					weather: response.data
-				})
+					weather: response.data,
+				});
 			})
-			.catch(err => console.log(err))
-	}
-
+			.catch((err) => console.log(err));
+	};
 
 	componentDidMount() {
 		this.getApodData();
-        this.getWeatherData();
+		this.getWeatherData();
 	}
 
-
-    render() {
-        const  picture  = this.state.apod;
-        const  solar  = this.state.weather;
-        console.log('3rd checking solar variable:', solar)
-        return (
-            <div>
-                <h2>Mars Life of User XYZ</h2>
-                <p>Welcome back, dear fellow Martian. As one of the first humans migrated to Mars, your journals are extremely valuable for the future generations to come.</p>
-                <img src = {picture} style={{width: '600px'}} alt='space photo'></img>
-                <h4>Attention, solar activities, plan your day accordingly</h4>
-                {solar.map(elm =>
-                <div key={elm.flrID}>
-                <p>Solar flare started at {elm.beginTime}, ended at {elm.endTime}.</p>
-                </div>
-                )}
-                <p>Time recorded using Earth time. Disruption to data could occur due to space weather activities</p>
-                <h2>place holder for journal entry</h2>
-            </div>
-        )
-    }
+	render() {
+		const picture = this.state.apod;
+		const solar = this.state.weather;
+		console.log("3rd checking solar variable:", solar);
+		return (
+			<div>
+				<h2>Mars Life of User XYZ</h2>
+				<p>
+					Welcome back, dear fellow Martian. As one of the first humans migrated
+					to Mars, your journals are extremely valuable for the future
+					generations to come.
+				</p>
+				<img src={picture} style={{ width: "600px" }} alt="space"></img>
+				<h4>Attention, solar activities, plan your day accordingly</h4>
+				{solar.map((elm) => (
+					<div key={elm.flrID}>
+						<p>
+							Solar flare started at {elm.beginTime}, ended at {elm.endTime}.
+						</p>
+					</div>
+				))}
+				<p>
+					Time recorded using Earth time. Disruption to data could occur due to
+					space weather activities
+				</p>
+				<h2>place holder for journal entry</h2>
+			</div>
+		);
+	}
 }
